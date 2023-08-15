60f300190925554f1adf12b8c1dd2cca3af20cf7
Ray Legion <ray.chen@virovek.com>
2023-08-15 12:21:20 -0700

    i hate typescript (sometimes)

--- context/AppContext.tsx
+++ context/AppContext.tsx
@@ -1,17 +1,23 @@
 import { NavigationProp, useNavigation } from '@react-navigation/native'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 import { Dispatch, SetStateAction, createContext, useContext, useState } from 'react'
 
 type AppContextType = {
   turbo: boolean 
   setTurbo: Dispatch<SetStateAction<boolean>>
-  navigation: NavigationProp<ReactNavigation.RootParamList>
+  navigation: NativeStackNavigationProp<RootStackParamList>
+}
+
+export type RootStackParamList = {
+  Main: undefined 
+  Credits: undefined 
 }
 
 const AppContext = createContext<AppContextType | null>(null)
 
 export const AppContextProvider = ({children} : {children: React.ReactNode}) => {
   const [turbo, setTurbo] = useState(false)
-  const navigation = useNavigation()
+  const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>()
 
   return (
     <AppContext.Provider value = {{
